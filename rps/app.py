7ef285a6209699ef50637647579d43f45b0325ba
Eric Lee <eric.pungho.lee@gmail.com>
2023-03-08 14:16:04 -0400

    Add files via upload

---
+++ rps/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, flash
+import random
+
+app = Flask(__name__)
+app.secret_key = "wastogi999"
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/rps", methods=["POST", "GET"])
+def rps():
+    choices = ["✊", "✋", "✌️"]
+    computer = random.choice(choices)
+    user = request.form.get("user")
+
+    if user == computer:
+        flash(f"Computer: {computer} | You: {user} | Tie!🥱")
+        return render_template("index.html")
+    elif (computer == "✊" and user == "✋") or (computer == "✋" and user == "✌️") or (computer == "✌️" and user == "✊"):
+        flash(f"Computer: {computer} | You: {user} | You've won!🎊")
+        return render_template("index.html")
+    else:
+        flash(f"Computer: {computer} | You: {user} | You've lost!👎")
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
